f59ec2c3fdfce043600d3bc0a653cbac47f84ad5
Cassie Wallace <cassbwallace@gmail.com>
2022-09-13 10:59:22 -0600

    Set up project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Cassie Wallace on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Memorize
+
+First application in coursework for [Stanford's CS193p (Developing Applications for iOS using SwiftUI)](https://cs193p.sites.stanford.edu/), Spring 2021 version.
+
